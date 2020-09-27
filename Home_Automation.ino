f179fe3f90da796b49a4dad5137dc8ac6fb57fed
Santosh Kumar Bhuyan <santoshbhuyan723@gmail.com>
2020-09-27 15:00:09 +0530

    Add files via upload

---
+++ Home_Automation.ino
@@ -0,0 +1,36 @@
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+
+#include <BlynkSimpleEsp8266.h>
+
+
+char auth[] = "8XS3CKhZJSE1ciB70Jqu5-E7SNeKEpGR;
+
+char ssid[] = "santosh";
+
+char pass[] = "santosh723";
+
+
+void setup()
+
+{
+
+// Debugging console
+
+Serial.begin(9600);
+
+Blynk.begin(auth, ssid, pass);
+
+Blynk.syncAll(); //This will sync the last state of our device
+
+}
+
+void loop()
+
+{
+
+Blynk.run();
+
+}
+
